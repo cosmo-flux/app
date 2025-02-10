809a8426018cde3d86a162f469fc1fa9319a273b
Eden <eden@bit.dev>
2025-02-10 23:09:21 +0200

    integrate mfes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { App, Frontend } from '@cosmo-flux/foundation.subscriber';
+import { marketingMfe } from '@cosmo-flux/marketing.marketing-mfe';
+import { bookingMfe } from '@cosmo-flux/booking.booking-mfe';
+
+const app = new App();
+const frontends: Frontend[] = [marketingMfe, bookingMfe];
+frontends.forEach((frontend) => frontend(app));
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>{app.renderApp()}</BrowserRouter>
+  </StrictMode>
+);
